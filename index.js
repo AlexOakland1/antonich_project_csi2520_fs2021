23fec870919e395a73f5f79bcefb6a60ac72adaf
Alex Antonich <antonich@oakland.edu>
2021-12-11 22:39:55 -0500

    final touches

--- index.js
+++ index.js
@@ -86,47 +86,6 @@ app.post("/logout", (req, res) => {
   res.render("index", {error: 0});
 });
 
-app.post("/insertstudents", (req, res) => {
-  let data = { name: req.body.studentName, email: req.body.studentEmail };
-  let sql = `INSERT INTO students SET ?`;
-  let query = db.query(sql, data, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.send(`student entry was inserted to the db...`);
-  });
-});
-
-app.post("/updatestudents", (req, res) => {
-  let sql = `UPDATE students SET email = '${req.body.studentNewEmailUpdate}'  WHERE id = ${req.body.studentID}`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.send(`student entry was updated in the db...`);
-  });
-});
-
-app.post("/deletestudents", (req, res) => {
-  let sql = `DELETE FROM students WHERE email = '${req.body.studentEmail}'`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.send(`student entry was deleted in the db...`);
-  });
-});
-
-app.get("/readstudents", (req, res) => {
-  let sql = `SELECT * FROM students`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.render("readData", { data: result });
-  });
-});
-
 // Setup server ports
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
