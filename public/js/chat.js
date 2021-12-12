327d97b8914eb81216fa87d0109511dc5a4ab461
Alex Antonich <antonich@oakland.edu>
2021-12-11 21:28:39 -0500

    getting chat to work

---
+++ public/js/chat.js
@@ -0,0 +1,11 @@
+let chatButton = document.querySelector('#upload');
+let chatSpace = document.querySelector('#chat');
+let chatBox = document.querySelector('#chatbox');
+let bar = document.querySelector('#containerbar');
+
+chatButton.addEventListener('click', function () {
+    let chatmsg = document.createElement('p');
+    let chatText = document.createTextNode(chatBox.value);
+    chatmsg.appendChild(chatText);
+    articleContainer.insertBefore(chatmsg, bar);
+})
