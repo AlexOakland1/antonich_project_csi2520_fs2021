21cdbc2603463e47213c550bc106b13926410c52
Alex Antonich <antonich@oakland.edu>
2023-11-06 19:29:40 -0500

    add basic encryption proof of concept

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,8 +4,43 @@ let chatBox = document.querySelector('#chatbox');
 let bar = document.querySelector('#containerbar');
 
 chatButton.addEventListener('click', function () {
+    // user's input
+    const userMessage = chatBox.value;
+
+    // encrypt user's message
+    const encryptionKey = 3;
+    const encryptedMessage = encryptMessage(userMessage, encryptionKey);
+
     let chatmsg = document.createElement('p');
-    let chatText = document.createTextNode(user + ":  " + chatBox.value);
+    let chatText = document.createTextNode(user + "(Encrypted message): " + encryptedMessage); // Display the encrypted message
     chatmsg.appendChild(chatText);
     chatSpace.insertBefore(chatmsg, bar);
-})
+
+   // decrypt and display message
+   const decryptedMessage = decryptMessage(encryptedMessage, encryptionKey);
+   let decryptedChatMsg = document.createElement('p');
+   let decryptedChatText = document.createTextNode(user + " (Decrypted message): " + decryptedMessage);
+   decryptedChatMsg.appendChild(decryptedChatText);
+   chatSpace.insertBefore(decryptedChatMsg, bar);
+});
+
+// encryption function
+function encryptMessage(message, key) {
+    let encryptedMessage = '';
+    for (let i = 0; i < message.length; i++) {
+        const charCode = message.charCodeAt(i);
+        const encryptedCharCode = charCode + key;
+        encryptedMessage += String.fromCharCode(encryptedCharCode);
+    }
+    return encryptedMessage;
+}
+
+// decryption function
+function decryptMessage(encryptedMessage, key) {
+    let decryptedMessage = '';
+    for (let i = 0; i < encryptedMessage.length; i++) {
+        const charCode = encryptedMessage.charCodeAt(i);
+        const decryptedCharCode = charCode - key;
+        decryptedMessage += String.fromCharCode(decryptedCharCode);
+    }
+    return decryptedMessage;
