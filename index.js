271abcc99ccbf1c894ed03ce4f5a93c6c44b551f
Alex Antonich <antonich@oakland.edu>
2023-12-12 00:26:00 -0500

    suffering

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let s3 = new aws.S3({
   database: process.env.JAWSDB_DB
 });
 
-console.log(s3);
+console.log(s3.process.env.JAWSDB_HOST);
 
 // get config from config/config.json
 //const dbConfig = config.get('Database.dbConfig');
