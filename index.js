d02bde6b8263bdbf1331baef7d223677ff358109
Alex Antonich <antonich@oakland.edu>
2023-12-12 20:09:41 -0500

    chat is working! no encryption yet

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const aws = require('aws-sdk');
 // Create express app
 const app = express();
 
-// import config variables from heroku
+// get config from heroku env vars
+import config variables from heroku
 let s3 = new aws.S3({
   host: process.env.JAWSDB_HOST,
   user: process.env.JAWSDB_USER,
@@ -22,13 +23,15 @@ let s3 = new aws.S3({
 // get config from config/config.json
 //const dbConfig = config.get('Database.dbConfig');
 
-// Create a database connection configuration
+//Create a database connection configuration based on heroku vars
 const db = mysql.createConnection({
   host: s3.config.host,
   user: s3.config.user,
   password: s3.config.password,
-  database: s3.config.database, // comment out if running example 1
+  database: s3.config.database,
 });
+
+// connect to db based on local config, only used for development
 //const db = mysql.createConnection(dbConfig);
 
 // Establish connection with the DB
@@ -58,58 +61,66 @@ app.get("/create_account", (req, res) => {
 });
 
 app.get("/chat", (req, res) => {
-    res.render("chat");
+  const selectMessages = "SELECT * FROM messages";
+  const selectUsernames = "SELECT DISTINCT username FROM messages ORDER BY username";
+
+  db.query(selectUsernames, (err, usernames) => {
+    if (err) {
+      console.error('Error fetching usernames:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    db.query(selectMessages, (err, messages) => {
+      if (err) {
+        console.error('Error fetching messages:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      res.send({data: messages, usernames: usernames});
+    });
+  });
 });
 
-app.post("/createaccount", async (req, res) => {
-  const username = req.body.username.trim();
-  const password = req.body.password.trim();
 
-  // Generate RSA key pair for the new user
-  const { publicKey, privateKey } = await generateKeyPairPromise('rsa', {
-    modulusLength: 2048,
-    publicKeyEncoding: { type: 'spki', format: 'pem' },
-    privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+app.post("/createaccount", (req, res) => {
+  let data = { username: req.body.username.trim(), password: req.body.password.trim() };
+  let data1 = req.body.username.trim();
+  let data2 = req.body.password.trim();
+  let sql = `INSERT INTO users SET ?`;
+  let sqllist = 'SELECT DISTINCT username FROM users';
+  let query = db.query(sql, data, (err, result) => {
+    if (err) {
+      res.render("create_account", { error: 1 });
+    } else {
+      // Fetch all messages from the database
+      const selectMessages = "SELECT * FROM messages";
+      db.query(selectMessages, (err, messages) => {
+        res.render("chat", { user: data1, data: messages });
+      });
+    }
   });
-
-  // Derive an encryption key from the user's password using a modern key derivation function
-  const key = crypto.scryptSync(password, 'salt', 32);
-
-  // Encrypt the private key with the derived key for added security
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
-  let encryptedPrivateKey = cipher.update(privateKey, 'utf-8', 'base64');
-  encryptedPrivateKey += cipher.final('base64');
-
-  // Store the user details, including the encrypted private key, in the database
-  const insertUserQuery = "INSERT INTO users (username, password, public_key, private_key) VALUES (?, ?, ?, ?)";
-  await queryDB(insertUserQuery, [username, password, publicKey, encryptedPrivateKey]);
-
-  // Fetch the list of users for chat display
-  const sqllist = 'SELECT username FROM users';
-  const userList = await queryDB(sqllist);
-
-  res.render("chat", { user: username, data: userList, publickey: publicKey });
 });
 
 app.post("/loginaccount", (req, res) => {
   let data1 = req.body.username.trim();
   let data2 = req.body.password.trim();
   let sql = `SELECT * FROM users WHERE username = ? AND password = ?`;
-  let sqllist =  'SELECT username FROM users';
+  let sqllist = 'SELECT DISTINCT username FROM users';
   let query = db.query(sql, [data1, data2], (err, result) => {
     if (err) {
       throw err;
     }
     if (result.length == 0) {
-      res.render("index", {error: 1});
+      res.render("index", { error: 1 });
     } else {
-      db.query(sqllist, (err, result2) => {
-        console.log(result[0].public_key)
-        res.render("chat", {user: data1, publickey: result, data: result2});
+      // Fetch all messages from the database
+      const selectMessages = "SELECT * FROM messages";
+      db.query(selectMessages, (err, messages) => {
+        res.render("chat", { user: data1, data: messages });
       });
     }
-    //console.log(result);
   });
 });
 
@@ -117,113 +128,32 @@ app.post("/logout", (req, res) => {
   res.render("index", {error: 0});
 });
 
-app.post("/publicKey", async (req, res) => {
-  const { user } = req.body;
-
-  try {
-    // Fetch the public key from the database based on the currently logged-in user
-    const publicKeyQuery = "SELECT public_key FROM users WHERE username = ?";
-    const publicKeyResult = await queryDB(publicKeyQuery, [user]);
-
-    if (publicKeyResult.length === 0) {
-      return res.status(404).json({ error: "Public key not found for the user" });
+// New endpoint to handle chat messages
+app.post("/sendmessage", (req, res) => {
+  const username = req.body.username.trim();
+  const message = req.body.message.trim();
+  const insertMessage = "INSERT INTO messages (username, message) VALUES (?, ?)";
+  db.query(insertMessage, [username, message], (err, result) => {
+    if (err) {
+      console.error('Error inserting message:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
     }
 
-    const publicKey = publicKeyResult[0].public_key;
-
-    // Convert the base64-encoded public key to an ArrayBuffer
-    const publicKeyArrayBuffer = base64ToBuffer(publicKey);
-
-    // Send the public key in the response
-    res.json({ publicKey: publicKeyArrayBuffer });
-  } catch (error) {
-    console.error("Error fetching public key:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-function base64ToBuffer(base64String) {
-  console.log("base64String:", base64String);
-
-  // Extract the base64-encoded part between the markers
-  const base64Data = base64String.match(/-----BEGIN PUBLIC KEY-----([\s\S]+)-----END PUBLIC KEY-----/)[1].replace(/\s/g, '');
-
-  console.log("base64Data:", base64Data);
-
-  // Use btoa to encode binary data to base64
-  const binaryString = atob(base64Data);
-  console.log("binaryString:", binaryString);
-
-  // Convert the binary string to a Uint8Array
-  const uint8Array = Uint8Array.from(binaryString, char => char.charCodeAt(0));
-
-  // Convert the Uint8Array to a buffer
-  const buffer = uint8Array.buffer;
-
-  console.log("buffer:", buffer);
-
-  return buffer;
-}
-
-app.post("/sendmessage", async (req, res) => {
-  const user = req.body.user;
-  const message = req.body.message;
-
-  // Fetch the user's private key from the database (make sure to store it securely)
-  const privateKeyQuery = "SELECT private_key FROM users WHERE username = ?";
-  const privateKeyResult = await queryDB(privateKeyQuery, [user]);
-
-  if (privateKeyResult.length === 0) {
-    return res.status(400).json({ error: "User not found or private key not available" });
-  }
-
-  const privateKey = privateKeyResult[0].private_key;
-
-  // Decrypt the user's private key
-  const decryptedPrivateKey = crypto.privateDecrypt(
-    {
-      key: Buffer.from(privateKey, 'base64'),
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    },
-    Buffer.from(privateKey, 'base64')
-  );
-
-  // Encrypt the message using the decrypted private key
-  const encryptedMessageBuffer = await crypto.subtle.encrypt(
-    {
-      name: 'RSA-OAEP',
-    },
-    await crypto.subtle.importKey(
-      'spki',
-      Buffer.from(decryptedPrivateKey).toString('base64'),
-      { name: 'RSA-OAEP', hash: { name: 'SHA-256' } },
-      true,
-      ['encrypt']
-    ),
-    new TextEncoder().encode(message)
-  );
-
-  const encryptedMessageBase64 = Buffer.from(encryptedMessageBuffer).toString('base64');
-
-  // Store the encrypted message in the database
-  const storeMessageQuery = "INSERT INTO messages (username, encrypted_message) VALUES (?, ?)";
-  await queryDB(storeMessageQuery, [user, encryptedMessageBase64]);
-
-  res.status(200).json({ success: true });
-});
-
-// Function to execute a database query
-function queryDB(sql, params) {
-  return new Promise((resolve, reject) => {
-    db.query(sql, params, (err, result) => {
+    // Fetch all messages from the database
+    const selectMessages = "SELECT * FROM messages";
+    db.query(selectMessages, (err, messages) => {
       if (err) {
-        reject(err);
-      } else {
-        resolve(result);
+        console.error('Error fetching messages:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
       }
+
+      res.json(messages); // Send messages as JSON
     });
   });
-}
+});
+
 
 // Setup server ports
 const PORT = process.env.PORT || 3000;
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,75 +1,60 @@
+let chatButton = document.querySelector('#upload');
 let chatSpace = document.querySelector('#chat');
 let chatBox = document.querySelector('#chatbox');
 let bar = document.querySelector('#containerbar');
 
-function encryptMessage(message, publicKey) {
-  return window.crypto.subtle.encrypt(
-    {
-      name: "RSA-OAEP"
-    },
-    publicKey,
-    new TextEncoder().encode(message)
-  );
-}
+// Function to fetch messages from the server and update the chat space
+function fetchMessages() {
+  fetch('/chat')
+    .then(response => response.json())
+    .then(data => {
+      // Clear existing chat messages and usernames
+      chatSpace.innerHTML = '';
+      usernameList.innerHTML = '';
 
-console.log(publickey);
-
-// Assuming the user and publicKey are obtained from the server
-let username = user; // Replace with the actual username
-let publicKey; // Replace with the actual public key received from the server
-
-// Fetch the public key before allowing the user to send messages
-async function fetchPublicKey() {
-  try {
-    const response = await fetch('/publicKey', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ user: username }) // Pass the current username
-    });
-
-    const data = await response.json();
-
-    console.log(data); // Log the data received from the server
-
-    if (data.publicKey) {
-      // Use btoa() to encode binary data to base64
-      publicKey = await crypto.subtle.importKey(
-        'spki',
-        btoa(String.fromCharCode.apply(null, new Uint8Array(data.publicKey))),
-        { name: 'RSA-OAEP', hash: { name: 'SHA-256' } },
-        true,
-        ['encrypt']
-      );
-    } else {
-      console.error("Public key not received from the server");
-    }
-  } catch (error) {
-    console.error("Error fetching public key:", error);
-  }
-}
-
-fetchPublicKey().then(() => {
-  let chatButton = document.querySelector('#upload');
-  chatButton.addEventListener("click", () => {
-    const userMessage = chatBox.value;
-    encryptMessage(userMessage, publicKey).then((encryptedMessageBuffer) => {
-      const encryptedMessageBase64 = Buffer.from(encryptedMessageBuffer).toString('base64');
-
-      // Send the encrypted message to the server
-      fetch('/sendmessage', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, message: encryptedMessageBase64 })
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          // Optionally, you can display a success message or handle the response accordingly
-          console.log("Message sent successfully");
-        } else {
-          console.error("Failed to send message");
-        }
+      console.log(data)
+      //Add fetched usernames to the username list
+      data.usernames.forEach(username => {
+        let usernameItem = document.createElement('li');
+        let usernameText = document.createTextNode(username.username);
+        usernameItem.appendChild(usernameText);
+        usernameList.appendChild(usernameItem);
       });
-    });
-  });
+
+      // Add fetched messages to the chat space
+      data.data.forEach(message => {
+        let chatmsg = document.createElement('p');
+        let chatText = document.createTextNode(`${message.username}: ${message.message}`);
+        chatmsg.appendChild(chatText);
+        chatSpace.appendChild(chatmsg);
+      });
+    })
+    .catch(error => console.error('Error fetching messages:', error));
+}
+
+chatButton.addEventListener('click', function () {
+  const chatMessage = chatBox.value;
+
+  // Assuming you have a global variable 'user' that contains the current user's username
+
+  // Use Fetch API to send the message to the server
+  fetch('/sendmessage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: user, message: chatMessage }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Fetch and update messages after sending a new message
+      fetchMessages();
+    })
+    .catch(error => console.error('Error sending message:', error));
+
+  // Clear the input box after sending the message
+  chatBox.value = '';
 });
+
+// Fetch messages when the page loads
+document.addEventListener('DOMContentLoaded', fetchMessages);
